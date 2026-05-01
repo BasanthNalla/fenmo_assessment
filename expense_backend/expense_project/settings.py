ccf7e30e78423f70c36b707a7025589bcfa0b6d6
BasanthNalla <basanthnalla987@gmail.com>
2026-05-01 13:28:02 +0530

    minor changes made

--- expense_backend/expense_project/settings.py
+++ expense_backend/expense_project/settings.py
@@ -27,9 +27,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -93,9 +93,13 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100
 }
 
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://fenmo-assessment-pjo7bat3t-basanth-s-projects.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
